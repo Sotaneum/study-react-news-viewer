87533b4979bc47cb0f3a7fd2e19818d518db976e
Sotaneum <gnyontu39@gmail.com>
2020-05-05 20:22:04 +0900

    Route 적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import React, { useCallback, useState } from 'react';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
-const App = () => {
-  const [category, setCategory] = useState('all');
-  const onSelect = useCallback((category) => setCategory(category), []);
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
-  return (
-    <>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </>
-  );
+const App = () => {
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
